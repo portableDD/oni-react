2c5e49323af98c5a0bf9d8ee2b2cae7958aa544a
portable <emmanueltemitopedorcas20@gmail.com>
2020-11-11 16:58:49 +0100

    done with react

--- oni/src/component/About.js
+++ oni/src/component/About.js
@@ -1,28 +1,42 @@
 import React from 'react'
 import Footer from './footer'
 import Animation from '../animation/animate'
+import Pics from '../pics/about.jpg'
 
 const About = () => {
     return(
         <div className = "hide-toggle-menu">
             <Animation />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget maximus sapien. 
-                Curabitur ultrices eros eu leo vestibulum, vitae rhoncus nisi eleifend. 
-                Sed rutrum diam eu interdum finibus. Phasellus venenatis tellus elit, pellentesque porttitor 
-                magna fringilla non. Proin eget accumsan dui. Integer tincidunt et enim vitae efficitur. 
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam maximus quam dolor, viverra 
-                fringilla mi maximus id. Nunc nec ex viverra, efficitur tortor non, ullamcorper urna. 
-                Donec id ipsum magna. Vivamus tincidunt ut sem ut faucibus. Ut quam felis, 
-                sollicitudin finibus sapien in, rhoncus efficitur felis. Integer at faucibus neque.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget maximus sapien. 
-                Curabitur ultrices eros eu leo vestibulum, vitae rhoncus nisi eleifend. 
-                Sed rutrum diam eu interdum finibus. Phasellus venenatis tellus elit, pellentesque porttitor 
-                magna fringilla non. Proin eget accumsan dui. Integer tincidunt et enim vitae efficitur. 
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam maximus quam dolor, viverra 
-                fringilla mi maximus id. Nunc nec ex viverra, efficitur tortor non, ullamcorper urna. 
-                Donec id ipsum magna. Vivamus tincidunt ut sem ut faucibus. Ut quam felis, 
-                sollicitudin finibus sapien in, rhoncus efficitur felis. Integer at faucibus neque.
-            </p>
+            <div className = "single-post">
+                <article className="post-section">
+                    <div className="single-index-thumbnails">
+                        <img src={Pics} alt="featured pic" />
+                        <h2 class="single-post-tittle">About Us</h2>
+                    </div>
+                    <div class="entry-content">
+                        <p>
+
+                        </p>
+                        <p class="MsoNormal">
+                            <span>
+                                Onibata mall is a platform that aims at connecting footwear makers and 
+                                their brands to potential customer through Digital marketing and offline
+                                marketing. Onibata mall aims at solving the problem of brand 
+                                visibility and market barriers that most foot wear makers are facing 
+                                by harnessing the power of technological innovation and media to help  
+                                footwear makers shift from being overlooked to overbooked.
+                                <br/>
+                                We also aim to contribute to the achievement of the number 8 of the 
+                                sustainable development goals by increasing decent work and economic 
+                                growth in Nigeria. We are certain that connecting foot wear makers to 
+                                customers and giving customers access to wide range of Nigerian made 
+                                products will make us achieve these milestones.
+                                <br/>
+                            </span>
+                        </p>
+                    </div>
+                </article>
+            </div>
             <Footer />
         </div>
     ) 
